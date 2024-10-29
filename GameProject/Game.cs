39a734b3c6e4ff95f94b6efd780286f677ed384f
darkdusk234 <johan.svensson.01@hotmail.com>
2024-10-29 14:46:08 +0100

    Created AddLevel with 1 overflow

--- GameProject/Game.cs
+++ GameProject/Game.cs
@@ -2,5 +2,16 @@
 {
     internal class Game
     {
+        List<Level> levels = new List<Level>();
+
+        public void AddLevel(Level level)
+        {
+            levels.Add(level);
+        }
+
+        public void AddLevel(Level level, int levelIndex)
+        {
+            levels.Insert(levelIndex, level);
+        }
     }
 }
