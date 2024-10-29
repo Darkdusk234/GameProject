14807367deee32895a052088985571a7bed172a9
jasharicoco <jashari.egzon@hotmail.com>
2024-10-29 14:59:10 +0100

    Player added

---
+++ GameProject/Player.cs
@@ -0,0 +1,15 @@
+﻿namespace GameProject
+{
+    internal class Player
+    {
+        private string _username { get; } = string.Empty;
+        private List<Game> gameLibrary;
+
+        public Player(string username)
+        {
+            _username = username;
+            gameLibrary = new List<Game>();
+        }
+
+    }
+}
