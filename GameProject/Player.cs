9065a4ea7e2716e5fddf2e2abfb3544555a9fc06
jasharicoco <jashari.egzon@hotmail.com>
2024-10-29 15:08:58 +0100

    Added gameConsole-list to Player

--- GameProject/Player.cs
+++ GameProject/Player.cs
@@ -4,11 +4,13 @@
     {
         private string _username { get; } = string.Empty;
         private List<Game> gameLibrary;
+        private List<GameConsole> gameConsole;
 
         public Player(string username)
         {
             _username = username;
             gameLibrary = new List<Game>();
+            gameConsole = new List<GameConsole>();
         }
 
     }
